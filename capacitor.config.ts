8a631cdbe1aa64934b2973a5ea62ca6408d960ed
Gunjan Mishra <Gunjan.Mishra-BZKY@qyupe.com>
2026-03-25 23:09:02 +0530

    added docker file for jenkins 1

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.example.gmportfolio",
+  appName: "GmPortfolio",
+  webDir: "public",
+  server: {
+    url: "https://portfolio.gunjanmishra.com/",
+  },
+};
+
+export default config;
